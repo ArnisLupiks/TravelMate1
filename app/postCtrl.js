04e280c50fe7def002736401c3ca060b3b6c24f5
Arnis <arnislupiks@gmail.com>
2015-01-20 12:37:42 +0000

    added filtering option in postPage
    added filter function to postPage, anabling option to search by words or
    numbers

--- app/postCtrl.js
+++ app/postCtrl.js
@@ -8,30 +8,39 @@ app.controller('postCtrl', function($scope, $http, $filter, $location) {
     });
  }
 
+ //filter utitlity function
+ $scope.customArrayFilter = function (item){
+   return (item.postHeading.indexOf('it') != -1);
+ };
+
 // option to order by date acsending and descenging mode
    var orderBy = $filter('orderBy');
      $scope.order = function(predicate, reverse){
        $scope.posts = orderBy($scope.posts, predicate, reverse);
      };
-   $scope.order('-postDate',false);
+
+     $scope.order('-postDate',false);
+
    //location scope is empty
-  $scope.location = '';
-// on submit button do post and collect data
-  $scope.submitForm = function() {
-// check if location is set or not
+   $scope.location = '';
+
+     // on submit button do post and collect data
+    $scope.submitForm = function() {
+
+            // check if location is set or not
             if($scope.location === ''){
                 alert('Directive did not update the location property in parent controller.');
             } else {
                 //alert('Yay. Location: ' + $scope.location);
             }
 
-    // adds results to post from *********************
-    var formData = {
-      uid: $('input[name=uid]').val(),
-      heading: $('input[name=heading]').val(),
-      content: $('input[name=content]').val(),
-      location: $scope.location
-    };
+      // adds results to post form *********************
+      var formData = {
+        uid: $('input[name=uid]').val(),
+        heading: $('input[name=heading]').val(),
+        content: $('input[name=content]').val(),
+        location: $scope.location
+      };
       $http({
           url: "api/posts/addPost.php",
           data: formData,
@@ -39,8 +48,9 @@ app.controller('postCtrl', function($scope, $http, $filter, $location) {
           headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
 
       }).success(function(data){
-
+          //if added information, it will log OK and redirect.
           console.log("OK", data);
+          //redirect to main post page
           $location.path("/");
 
       }).error(function(err){"ERR", console.log(err)})
--- index.php
+++ index.php
@@ -13,8 +13,8 @@
             <link href="css/toaster.css" rel="stylesheet">
 
             <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+            <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
 
 
